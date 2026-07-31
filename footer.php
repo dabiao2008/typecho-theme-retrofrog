c3c1814a925496cb87041fc29db40fe1014763d8
root <root@localhost.localdomain>
2026-07-31 10:59:25 +0800

    修改footer.php指向本项目

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-    </main>
+</main>
     <footer class="site-footer">
       <div class="footer-inner">
         <?php if ($this->options->copyright): ?>
@@ -6,7 +6,7 @@
         <?php else: ?>
           <p>&copy; <?php echo date('Y'); ?> <?php $this->options->title(); ?></p>
         <?php endif; ?>
-        <p>Powered by <a href="http://typecho.org/" target="_blank" rel="noopener">Typecho</a> &middot; Theme <a href="https://github.com/finch-xu/hexo-theme-warmpaper" target="_blank" rel="noopener">Warmpaper</a></p>
+        <p>Powered by <a href="http://typecho.org/" target="_blank" rel="noopener">Typecho</a> &middot; Theme <a href="https://github.com/dabiao2008/typecho-theme-retrofrog" target="_blank" rel="noopener">RetroFrog</a></p>
       </div>
     </footer>
   </div>
